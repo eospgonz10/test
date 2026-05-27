6e9f1eba7d0fc9ad2bc7b3474486cf1ad7ce0a6d
christian-gomezp <christian.gomezp@udea.edu.co>
2026-05-26 19:50:15 -0500

    Add sleep function to page object
    Added a sleep function to pause execution for a specified duration.

--- features/pageobjects/page.js
+++ features/pageobjects/page.js
@@ -1,14 +1,16 @@
 /**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
+ * main page object containing all methods, selectors and functionality
+ * that is shared across all page objects
+ */
 import { config } from "../../wdio.conf.js";
 export default class Page {
-    /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    open (path) {
-        return browser.url(`${config.baseUrl}/${path}.htm`);
-    }
+  /**
+   * Opens a sub page of the page
+   * @param path path of the sub page (e.g. /path/to/page.html)
+   */
+  open(path) {
+    return browser.url(`${config.baseUrl}/${path}.htm`);
+  }
 }
+
+export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
